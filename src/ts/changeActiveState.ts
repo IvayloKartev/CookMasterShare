8718307a7eecc4d0206465eda4d5ac0a58b6616a
Ivaylo Kartev <92308882+IvayloKartev@users.noreply.github.com>
2023-11-24 17:57:20 +0200

    Update changeActiveState.ts

--- src/ts/changeActiveState.ts
+++ src/ts/changeActiveState.ts
@@ -1,10 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
     const sideBarOptions :any = document.getElementsByClassName('sidebaroptions');
-    function switchState(){
-        
-    }
-    for(let i : number = 0; i<sideBarOptions.length; i++){
-        sideBarOptions[i].addEventListener("click", switchState);
-    }
+    let counter : number = 0;
+    let currentActiveIndex : number = 0;
 
+    function clickEvent(index){
+        if(counter === 0){
+            sideBarOptions[0].setAttribute("class", "nav-link text-white sideoptions");
+        }
+        currentActiveIndex = index;
+        sideBarOptions[currentActiveIndex].setAttribute("class", "nav-link bg-warning text-black");
+        counter++;
+    }
+    
+    for(let i : number = 0; i<sideBarOptions.array.length; i++){
+        sideBarOptions[i].addEventListener("click", () => {
+            clickEvent(i);
+        });
+    }
 })
